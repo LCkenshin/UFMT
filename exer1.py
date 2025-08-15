98ca53029cef3e813d0a35c0cd09cb1afde19beb
LCkenshin <98620804+LCkenshin@users.noreply.github.com>
2025-08-15 10:16:57 -0300

    exer1.py

--- exer1.py
+++ exer1.py
@@ -1,9 +1,14 @@
+#Exercicio 1
+#
+
+#Exercicio 2
 #Escreva 2 n | leia | some,multiplique,divida,subtraia
 
 def main():
 
-a=int(input("Digite o n1"))
-b=int(input("Digite o n2"))
+a=int(input("Digite o n1: "))
+b=int(input("Digite o n2: "))
+print("Os valores digitados foram: ",a,b)
 
 soma = a + b
 print("A soma dos valores é: ", soma)
@@ -18,4 +23,47 @@ div = a / b
 print("A soma dos valores é: ", div)
 
 if __name__="__main__":
+  main()
+
+#Exercicio 3
+#leia 3 valor determine maior menor e igual
+
+def main():
+    a = int(input("Digite n1: "))
+    b = int(input("Digite n2: "))
+    c = int(input("Digite n3: "))
+    print("Os valores são: ", a, " ", b, " ", c)
+
+    #maior e menor
+    maior = a
+    menor = b
+    
+    # maior valor
+    if a > b and a > c:
+        maior = a
+    elif b > a and b > c:
+        maior = b
+    else:
+        maior = c
+
+    # menor valor
+    if a < b and a < c:
+        menor = a
+    elif b < a and b < c:
+        menor = b
+    else:
+        menor = c
+
+    print("O maior valor é ", maior, "e o menor é ", menor)
+
+if __name__ == "__main__":
+    main()
+
+  
+#Exercicio 4
+#Leia 3 valores e tire a media deles | verifcar qual é o menor e maior numero digitado
+
+def main():
+
+if __name__=="__main__"
   main()
