d95c27343f3614a7de75db14ee21b548967cffed
LCkenshin <98620804+LCkenshin@users.noreply.github.com>
2025-08-15 08:57:11 -0300

    Add files via upload

---
+++ exer1.py
@@ -0,0 +1,21 @@
+#Escreva 2 n | leia | some,multiplique,divida,subtraia
+
+def main():
+
+a=int(input("Digite o n1"))
+b=int(input("Digite o n2"))
+
+soma = a + b
+print("A soma dos valores é: ", soma)
+
+mult = a * b
+print("A multiplicação dos valores é: ", mult)
+
+sub = a - b
+print("A soma dos valores é: ", sub)
+
+div = a / b
+print("A soma dos valores é: ", div)
+
+if __name__="__main__":
+  main()
